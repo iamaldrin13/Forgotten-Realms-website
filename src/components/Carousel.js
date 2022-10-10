0f335b2c6e6e51a556484c3f8f29e1fd52512083
iamaldrin13 <aldrin.roxas08@gmail.com>
2022-10-10 18:38:55 +0800

    Swiper export

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import 'swiper/swiper-bundle.css';
+import 'swiper/swiper-bundle.min.css';
 
 
  // Import Swiper React components
